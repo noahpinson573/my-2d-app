03d63ef39107858b874f47c94a555f6d55760dc6
noahpinson573 <noahpinson573@gmail.com>
2025-12-21 18:12:18 -0500

    Change cache version and add assets for caching
    Updated cache name and asset list in service worker.

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,27 @@
-const CACHE = "app-v1";
+const CACHE = "neon-arcade-v3";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
 
 self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
   e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json"
-      ])
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
