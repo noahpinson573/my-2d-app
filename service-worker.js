bde784b1f3dea61a0c969af389f4cbf7e6925df2
noahpinson573 <noahpinson573@gmail.com>
2025-12-22 08:12:59 -0800

    Refactor service worker for versioning and caching

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,6 @@
-const CACHE="neon-hub-v2";
-const ASSETS=[
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
-];
-
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate",e=>{
-  e.waitUntil(
-    caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+const APP_VERSION = "1.0.0";
+const CACHE_NAME = `neon-games-${APP_VERSION}`;
+const ASSETS = ["/","/index.html","/manifest.json","/icon-192.png","/icon-512.png"];
+self.addEventListener("install", e => {e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener("activate", e => {e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch", e => {e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request).then(r=>caches.open(CACHE_NAME).then(c=>{c.put(e.request,r.clone());return r;})).catch(()=>caches.match("/index.html"))));});
