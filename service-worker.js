a1904dbd48a6a3303f71e75e3fa5806ce0e2f5a1
noahpinson573 <noahpinson573@gmail.com>
2025-12-24 14:37:52 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,22 @@
-const CACHE="neon-cache-v5";
+const CACHE="neon-cache-v9";
 const FILES=[
   "./","./index.html","./style.css","./hub.js",
-  "./manifest.json","./icon-192.png","./icon-512.png",
-  "./game-snake.js","./game-tetris.js","./game-platformer.js",
-  "./game-pong.js","./game-breakout.js"
+  "./manifest.json",
+  "./game-platformer.js","./game-snake.js",
+  "./game-tetris.js","./game-pong.js","./game-breakout.js",
+  "./icon-192.png","./icon-512.png"
 ];
 
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
   self.skipWaiting();
 });
+
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>x!==CACHE&&caches.delete(x)))));
+  self.clients.claim();
+});
+
 self.addEventListener("fetch",e=>{
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
