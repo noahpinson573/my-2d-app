e1fcba6b84bbfa49992d9552aef6483e8698a83c
noahpinson573 <noahpinson573@gmail.com>
2025-12-23 19:32:43 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,21 @@
-const CACHE="neon-cache-v5";
-const FILES=[
-  "./","./index.html","./style.css","./hub.js",
-  "./manifest.json","./icon-192.png","./icon-512.png",
-  "./game-snake.js","./game-tetris.js","./game-platformer.js",
-  "./game-pong.js","./game-breakout.js"
-];
-
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
-  self.skipWaiting();
-});
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+Games.snake={
+  init(){this.s=[{x:10,y:10}];this.d={x:0,y:0};this.f={x:5,y:5};this.c=0;},
+  update(){
+    this.c++; if(this.c<6)return; this.c=0;
+    if(keys.ArrowLeft)this.d={x:-1,y:0};
+    if(keys.ArrowRight)this.d={x:1,y:0};
+    if(keys.ArrowUp)this.d={x:0,y:-1};
+    if(keys.ArrowDown)this.d={x:0,y:1};
+    const h={x:this.s[0].x+this.d.x,y:this.s[0].y+this.d.y};
+    this.s.unshift(h);
+    if(h.x===this.f.x&&h.y===this.f.y){
+      this.f={x:Math.floor(Math.random()*20),y:Math.floor(Math.random()*20)};
+    } else this.s.pop();
+  },
+  draw(){
+    ctx.fillStyle="#00ff95";
+    this.s.forEach(p=>ctx.fillRect(p.x*20,p.y*20,20,20));
+    ctx.fillStyle="#ff0066";
+    ctx.fillRect(this.f.x*20,this.f.y*20,20,20);
+  }
+};
