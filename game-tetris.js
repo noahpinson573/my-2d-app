f1d7f2f241b34bb14d3804480ed05ed886c0ff18
noahpinson573 <noahpinson573@gmail.com>
2025-12-24 14:36:38 -0500

    Update game-tetris.js

--- game-tetris.js
+++ game-tetris.js
@@ -1,26 +1,26 @@
-Games.tetris = {
+Games.tetris={
   init(){
-    this.w=10; this.h=20; this.s=30;
+    this.w=10;this.h=20;this.s=30;
     this.grid=[...Array(this.h)].map(()=>Array(this.w).fill(0));
     this.shapes=[[[1,1,1,1]],[[1,1],[1,1]],[[0,1,0],[1,1,1]]];
     this.colors=["#00ff95","#ffb000","#ff0066"];
-    this.spawn(); this.t=0;
+    this.spawn();this.t=0;
   },
   spawn(){
     const i=Math.floor(Math.random()*this.shapes.length);
     this.p={s:this.shapes[i],x:3,y:0,c:this.colors[i]};
   },
   update(){
-    if(++this.t<15)return; this.t=0;
+    if(++this.t<15)return;this.t=0;
     this.p.y++;
-    if(this.hit()){this.p.y--; this.merge(); this.spawn();}
-    if(keys.ArrowLeft){this.p.x--; if(this.hit())this.p.x++; keys.ArrowLeft=false;}
-    if(keys.ArrowRight){this.p.x++; if(this.hit())this.p.x--; keys.ArrowRight=false;}
+    if(this.hit()){this.p.y--;this.merge();this.spawn();}
+    if(keys.ArrowLeft){this.p.x--;if(this.hit())this.p.x++;keys.ArrowLeft=false;}
+    if(keys.ArrowRight){this.p.x++;if(this.hit())this.p.x--;keys.ArrowRight=false;}
   },
   hit(){
     return this.p.s.some((r,y)=>r.some((v,x)=>{
       if(!v)return false;
-      const gx=this.p.x+x, gy=this.p.y+y;
+      const gx=this.p.x+x,gy=this.p.y+y;
       return gx<0||gx>=this.w||gy>=this.h||this.grid[gy][gx];
     }));
   },
